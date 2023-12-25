47e1a73a52a39697ba758375d0191a1e7f502619
c0derdx <coderdx007@gmail.com>
2023-12-25 18:21:23 +0530

    final implementation

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const app = express();
-const port = 8000;
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require('./routes')
+const cors = require('cors')
+require("dotenv").config()
+const app = express()
+const port = 8000
 
-                                                                                           
-//MongoDb connection
-mongoose
-  .connect(
-    "mongodb+srv://admin:" +
-      process.env.MONGO_PASS +
-      "@cluster0.ulobx.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then((x) => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    console.log("error");
-  });
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:5173' }));
+const startMongo = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://admin:" + "Khatu11rs" + "@cluster0.ulobx.mongodb.net/?retryWrites=true&w=majority", {})
+    console.log("Connected to MongoDB")
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error)
+    process.exit(1)
+  }
+}
 
+startMongo()
 
+app.use("/", routes)
 
 app.listen(port, () => {
-  console.log("server has started on port " + port);
-});
-
+  console.log("server has started on port ", port)
+})
--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
+const express = require("express")
+const router = express.Router()
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
 
 const createErrorResponse = (message) => ({
   error: message,
-});
+})
 
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email })
 
     if (existingUser) {
-      return res.status(403).json(createErrorResponse("User already registered with this email"));
+      return res.status(403).json(createErrorResponse("User already registered with this email"))
     }
 
-    const newUser = await User.create({ email, password });
-    return res.status(200).json(newUser);
+    const newUser = await User.create({ email, password })
+    const token = generateToken(newUser)
+    return res.status(200).json({ user: newUser, token })
   } catch (error) {
-    console.error("Error during registration:", error);
-    return res.status(500).json(createErrorResponse("Internal Server Error"));
+    console.error("Error during registration:", error)
+    return res.status(500).json(createErrorResponse("Internal Server Error"))
   }
-});
+})
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -31,14 +33,29 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user || user.password !== password) {
-      return res.status(403).json(createErrorResponse("Invalid credentials"));
+      return res.status(403).json(createErrorResponse("Invalid credentials"))
     }
 
-    return res.status(200).json(user);
+    const token = generateToken(user)
+    return res.status(200).json({ user, token })
   } catch (error) {
-    console.error("Error during login:", error);
-    return res.status(500).json(createErrorResponse("Internal Server Error"));
+    console.error("Error during login:", error)
+    return res.status(500).json(createErrorResponse("Internal Server Error"))
   }
-});
+})
 
-module.exports = router;
+// Helper function to generate a JWT token
+function generateToken(user) {
+  const payload = {
+    userId: user._id,
+    email: user.email,
+  }
+
+  const secretKey = "yourSecretKey"
+  const expiration = "1h"
+
+  const token = jwt.sign(payload, secretKey, { expiresIn: expiration })
+  return token
+}
+
+module.exports = router
